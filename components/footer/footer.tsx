f631976434798029d62c498e25e4e5caebb98c40
Gabriel Tonete <gabtopi77@gmail.com>
2022-04-12 12:29:14 -0300

    [Feature] Componentes de Header & Footer

---
+++ components/footer/footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+    return (
+        <footer>
+            <h1>ooter</h1>F
+        </footer>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next'
+import { Header } from '@/components';
 
 const Home: NextPage = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Header />
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
   )
 }
 
